779a920b8a192cc9943f1806566f803771d46301
Phillip Deremer <deremer@orbitlog.com>
2026-08-14 10:25:59 +0200

    feat: refactor base_Functional and extend functionality for arithmetic and comparison operators

--- js/operators/functional.js
+++ js/operators/functional.js
@@ -3,30 +3,10 @@ class base_Functional extends Operator {
 	constructor(x = 0, y = 0) {
 		super(x, y)
 
-		this.in_b1 = this.newInput("B1")
-		this.in_b2 = this.newInput("B2")
-		this.out_r = this.newOutput("R")
-		this.out_o = this.newOutput("O")
-		this.out_no = this.newOutput("!O")
+		this.in_b1 = this.newInput("B1") // Byte 1
+		this.in_b2 = this.newInput("B2") // Byte 2 
 
-		this.func = (a, b) => 0
-		this.outp = (a, b) => false
 		this.icon = ""
-	}
-
-	doUpdate(tick, p5ctx) {
-		super.doUpdate(tick, p5ctx)
-
-		let a = (this.in_b1.value) & 255
-		let b = (this.in_b2.value) & 255
-
-		let r = this.func(a, b)
-    let o = this.outp(a, b)
-		let no = !o
-
-		this.out_r.value = r
-		this.out_o.value = o
-		this.out_no.value = no
 	}
 
 	doDraw(tick, p5ctx) {
@@ -44,44 +24,150 @@ class base_Functional extends Operator {
 	}
 }
 
-const Op_Equals = register(
-	"Equals",
-	"O = B1 == B2",
-	class extends base_Functional {
-		constructor(x = 0, y = 0) {
-			super(x, y)
+class base_Functional_calc extends base_Functional {
 
-			this.func = (a, b) => a ^ b
-			this.outp = (a, b) => a == b
-			this.icon = "EQ"
-		}
+	constructor(x = 0, y = 0) {
+		super(x, y)
+
+		this.out_r = this.newOutput("R") // Result
+		this.out_rn = this.newOutput("RN") // is Result Negative
+		this.out_o = this.newOutput("O") // Overflow
+		this.out_on = this.newOutput("ON") // is Overflow Negative
+
+		this.func = (a, b) => [0] // Should Return R because RN O ON are defined by the base!
+
+		this.icon = ""
 	}
-)
+
+	doUpdate(tick, p5ctx) {
+		super.doUpdate(tick, p5ctx)
+
+		let b1 = (this.in_b1.value) & 255
+		let b2 = (this.in_b2.value) & 255
+
+		let r = this.func(b1, b2)
+
+		let rn = r < 0
+		let on = false
+		if (rn) {
+			r *= -1
+			on = true
+		}
+		let o = 0
+		if (r > 255) {
+			o = r - 255
+			r -= o
+		}
+		else {
+			on = false
+		}
+
+		this.out_r.value = r
+		this.out_rn.value = rn
+		this.out_o.value = o
+		this.out_on.value = on
+	}
+}
+
+class base_Functional_comparer extends base_Functional {
+
+	constructor(x = 0, y = 0) {
+		super(x, y)
+
+		this.out_b = this.newOutput("B") // Bitwise Output
+		this.out_o = this.newOutput("O") // Output
+		this.out_no = this.newOutput("!O") // Not Output
+
+		// (this placeholder function, purposly, has an invalid output with O and !O both being false to identify it as a placeholder)
+		this.func = (a, b) => [0, false, false] // Should Return B O !O in that order!
+
+		this.icon = ""
+	}
+
+	doUpdate(tick, p5ctx) {
+		super.doUpdate(tick, p5ctx)
+
+		let b1 = (this.in_b1.value) & 255
+		let b2 = (this.in_b2.value) & 255
+
+		let [b, o, no] = this.func(b1, b2)
+
+		this.out_b.value = b
+		this.out_o.value = o
+		this.out_no.value = no
+	}
+}
 
 const Op_Add = register(
 	"Add",
 	"O = B1 + B2",
-	class extends base_Functional {
+	class extends base_Functional_calc {
 		constructor(x = 0, y = 0) {
 			super(x, y)
 
-			this.func = (a, b) => (a + b) & 255
-			this.outp = (a, b) => !!(a + b)
+			this.func = (a, b) => (a + b)
+
 			this.icon = "+"
 		}
 	}
 )
 
 const Op_Sub = register(
-	"Sub",
+	"Subtract",
 	"O = B1 - B2",
-	class extends base_Functional {
+	class extends base_Functional_calc {
 		constructor(x = 0, y = 0) {
 			super(x, y)
 
-			this.func = (a, b) => (a - b) & 255
-			this.outp = (a, b) => !!(a - b)
+			this.func = (a, b) => (a - b)
+
 			this.icon = "-"
+		}
+	}
+)
+
+const Op_Mult = register(
+	"Multiply",
+	"O = B1 * B2",
+	class extends base_Functional_calc {
+		constructor(x = 0, y = 0) {
+			super(x, y)
+
+			this.func = (a, b) => (a * b)
+
+			this.icon = "\u00d7" // ×
+		}
+	}
+)
+
+const Op_Divide = register(
+	"Divide",
+	"O = B1 / B2",
+	class extends base_Functional_calc {
+		constructor(x = 0, y = 0) {
+			super(x, y)
+
+			this.func = (a, b) => (a / b)
+
+			this.icon = "/"
+		}
+	}
+)
+
+const Op_Equals = register(
+	"Equals",
+	"O = B1 == B2",
+	class extends base_Functional_comparer {
+		constructor(x = 0, y = 0) {
+			super(x, y)
+
+			this.func = (a, b) => [
+				(a ^ b) & 255,
+				a == b,
+				!(a == b)
+			]
+
+			this.icon = "EQ"
 		}
 	}
 )
