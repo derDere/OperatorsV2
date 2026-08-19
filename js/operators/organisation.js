46fbb767185ec4a9d7c5f1da8d3413c97acd5844
Phillip Deremer <deremer@orbitlog.com>
2026-08-19 18:00:32 +0200

    feat: add Op_Anchor operator with customizable title and dimensions

--- js/operators/organisation.js
+++ js/operators/organisation.js
@@ -104,3 +104,107 @@ const Op_Comment = register(
     }
   }
 )
+
+const Op_Anchor = register(
+  "Anchor",
+  "Organisation",
+  "Place this anywere on your canvas to mark the location and find those same coordinate again.",
+  class extends Operator {
+
+    constructor(x = 0, y = 0) {
+      super(x, y)
+
+      this.isAnchor = true
+
+      this.height = 40
+
+      this.borderColor = mainP5.color('transparent')
+      this.backgroundColor = mainP5.color('transparent')
+      this.backgroundHoverColor = mainP5.color('#ffffff30')
+      this.backgroundActiveColor = mainP5.color('#00000030')
+
+      this.size = 14
+      this.title = "New Anchor (" + x + ", " + y + ")"
+    }
+
+    getConfig() {
+      return {
+        ...super.getConfig(),
+        Title: this.title,
+        "Font Size": this.size
+      }
+    }
+
+    setConfig(conf, loaded = false) {
+      super.setConfig(conf, loaded)
+      if ('Title' in conf) {
+        this.title = conf.Title
+      }
+      if ('Font Size' in conf) {
+        this.size = conf["Font Size"]
+      }
+    }
+
+    _FontStyle(p5ctx) {
+      p5ctx.noStroke()
+      p5ctx.fill('#4a4079')
+      p5ctx.textStyle(p5ctx.BOLD)
+      p5ctx.textFont("Comic Sans MS")
+      p5ctx.textAlign(p5ctx.LEFT, p5ctx.TOP)
+      p5ctx.textSize(this.size)
+    }
+
+    getDragPos() {
+      let x = -(this.pos.x - (this.width / 2))
+      let y = -(this.pos.y + (this.height / 2))
+
+      x -= (mainP5.width / 2) - 2
+      y += (mainP5.height / 2) - 1
+
+      return [x, y]
+    }
+
+    doUpdate(tick, p5ctx) {
+      super.doUpdate(tick, p5ctx)
+
+      p5ctx.push()
+      this._FontStyle(p5ctx)
+      
+		  let bounds = p5ctx.textBounds(this.title, 0, 0);
+
+      let w = p5ctx.ceil((bounds.w + 30) / 20) * 20
+      let h = p5ctx.ceil((bounds.h + 10) / 20) * 20
+
+      if (w < 80) w = 80
+      if (h < 40) h = 40
+
+      this.width = w
+      this.height = h
+
+      p5ctx.pop()
+    }
+
+    doDraw(tick, p5ctx) {
+      super.doDraw(tick, p5ctx)
+
+      p5ctx.push()
+
+      this._FontStyle(p5ctx)
+
+      p5ctx.text(this.title, (-this.width / 2) + 15, -5)
+
+      p5ctx.noFill()
+      p5ctx.stroke('#4a4079')
+      p5ctx.line(
+        (-this.width / 2) - 1, (this.height / 2) + 1,
+        (-this.width / 2) - 1, (this.height / 2) - (p5ctx.height / 2) + 1,
+      )
+      p5ctx.line(
+        (-this.width / 2) - 1, (this.height / 2) + 1,
+        (-this.width / 2) - 1 + (p5ctx.width / 2), (this.height / 2) + 1,
+      )
+
+      p5ctx.pop()
+    }
+  }
+)
