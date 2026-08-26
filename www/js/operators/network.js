818ac043069672238d43324c7878508f3fb2a416
Phillip Deremer <deremer@orbitlog.com>
2026-08-26 10:48:58 +0200

    refactor: add GUID validation and enhance channel display functionality

--- www/js/operators/network.js
+++ www/js/operators/network.js
@@ -1,23 +1,89 @@
-const Op_NetSender = register(
-	"Network Sender",
-	"Network",
-	"Sends 1 byte into a defined network channel",
-	class extends Operator {
+function generateStringGuid(str) {
+  // 1. Einfacher Hash-Algorithmus (DJB2) zur Zustandserzeugung
+  let hash1 = 5381
+  let hash2 = 8933
 
-		constructor(x = 0, y = 0) {
-			super(x, y)
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i)
+    hash1 = ((hash1 << 5) + hash1) ^ char
+    hash2 = ((hash2 << 5) + hash2) ^ char
+  }
+
+  // 2. Buffer aus den Hashes erstellen (32-Bit-Konvertierung)
+  const b1 = Math.abs(hash1).toString(16).padStart(8, '0')
+  const b2 = Math.abs(hash2).toString(16).padStart(8, '0')
+  const b3 = Math.abs(hash1 ^ hash2).toString(16).padStart(8, '0')
+  const b4 = Math.abs(hash1 & hash2).toString(16).padStart(8, '0')
+
+  // 3. String zusammensetzen
+  const part1 = b1
+  const part2 = b2.substring(0, 4)
+  // UUID v4 Vorgabe: Das erste Zeichen im 3. Block muss eine '4' sein
+  const part3 = '4' + b3.substring(1, 4)
+  // UUID v4 Vorgabe: Das erste Zeichen im 4. Block muss '8', '9', 'a' oder 'b' sein
+  const allowedChars = ['8', '9', 'a', 'b']
+  const part4 = allowedChars[Math.abs(hash2) % 4] + b4.substring(1, 4)
+  const part5 = b1.substring(4, 8) + b2.substring(4, 8)
+
+  return `${part1}-${part2}-${part3}-${part4}-${part5}`
+}
+
+function isValidGuid(guid) {
+  // Regulärer Ausdruck für das standardmäßige UUID/GUID Format
+  const guidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
+  return guidRegex.test(guid)
+}
+
+function channelDisplay(guid, p5ctx) {
+  if (!isValidGuid(guid)) {
+    guid = generateStringGuid(guid)
+  }
+  guid = guid.replace(/-/g, '').toLowerCase()
+
+  const parts = []
+
+  for (let i = 0; i < guid.length; i+=2) {
+    let part = guid.substr(i, 2);
+    part = '#00' + part[0] + '0' + part[1] + '0'
+    parts.push(part)
+  }
+
+  p5ctx.push()
+  p5ctx.rectMode(p5ctx.CORNER)
+  p5ctx.translate(-20, -20)
+  p5ctx.noStroke()
+
+  for (let y = 0; y < 4; y++) {
+    for (let x = 0; x < 4; x++) {
+      let i = y * 4 + x
+      p5ctx.fill(parts[i])
+      p5ctx.rect((x * 10) + 0.5, (y * 10) + 0.5, 10, 10)
+    } 
+  }
+  
+  p5ctx.pop()
+}
+
+const Op_NetSender = register(
+  "Network Sender",
+  "Network",
+  "Sends 1 byte into a defined network channel",
+  class extends Operator {
+
+    constructor(x = 0, y = 0) {
+      super(x, y)
 
       this.channel = NewId()
 
       this._connected = false
 
-			this.in_b = this.newInput("B", "Byte", "Byte value sent into the channel on a trigger")
+      this.in_b = this.newInput("B", "Byte", "Byte value sent into the channel on a trigger")
       this.in_t = this.newInput("T", "Trigger", "A rising edge sends the current byte")
 
       this._lastT = false
 
       this._ws = new ChannelSocket(this.channel, 'send')
-		}
+    }
 
     kill() {
       super.kill()
@@ -25,22 +91,26 @@ const Op_NetSender = register(
     }
 
     getConfig() {
-			return {
-				...super.getConfig(),
-				Channel: this.channel
-			}
-		}
+      return {
+        ...super.getConfig(),
+        Channel: this.channel
+      }
+    }
 
-		setConfig(conf, loaded = false) {
-			super.setConfig(conf, loaded)
-			if ('Channel' in conf) {
-				this.channel = conf.Channel
-				this._ws.setChannel(this.channel)
-			}
-		}
+    setConfig(conf, loaded = false) {
+      super.setConfig(conf, loaded)
+      if ('Channel' in conf) {
+        let channelValue = conf.Channel
+        if (!isValidGuid(channelValue)) {
+          channelValue = generateStringGuid(channelValue)
+        }
+        this.channel = channelValue
+        this._ws.setChannel(this.channel)
+      }
+    }
 
-		doUpdate(tick, p5ctx) {
-			super.doUpdate(tick, p5ctx)
+    doUpdate(tick, p5ctx) {
+      super.doUpdate(tick, p5ctx)
 
       this._connected = this._ws.isConnected
 
@@ -56,10 +126,12 @@ const Op_NetSender = register(
       if (send) {
         this._ws.send(b)
       }
-		}
+    }
 
     doDraw(tick, p5ctx) {
-			super.doDraw(tick, p5ctx)
+      super.doDraw(tick, p5ctx)
+
+      channelDisplay(this.channel, p5ctx)
 
       p5ctx.push()
       if (this._connected) {
@@ -82,10 +154,9 @@ const Op_NetSender = register(
         0, 0,
         30, this.height,
         p5ctx.PI * 0.5,
-        p5ctx.PI * 1.5,
-        true
+        p5ctx.PI * 1.5
       )
-    
+
       p5ctx.fill(255)
       p5ctx.stroke(0)
       p5ctx.translate(10, 0)
@@ -94,8 +165,7 @@ const Op_NetSender = register(
         0, 0,
         30, 40,
         p5ctx.PI * 0.5,
-        p5ctx.PI * 1.5,
-        true
+        p5ctx.PI * 1.5
       )
       p5ctx.ellipse(
         0, 0,
@@ -133,23 +203,23 @@ const Op_NetSender = register(
 
       p5ctx.pop()
     }
-	}
+  }
 )
 
 const Op_NetReceiver = register(
-	"Network Receiver",
-	"Network",
-	"Receives 1 byte from a defined network channel",
-	class extends Operator {
+  "Network Receiver",
+  "Network",
+  "Receives 1 byte from a defined network channel",
+  class extends Operator {
 
-		constructor(x = 0, y = 0) {
-			super(x, y)
+    constructor(x = 0, y = 0) {
+      super(x, y)
 
       this.channel = NewId()
 
       this._connected = false
 
-			this.out_b = this.newOutput("B", "Byte", "The byte received last from the channel")
+      this.out_b = this.newOutput("B", "Byte", "The byte received last from the channel")
       this.out_t = this.newOutput("T", "Trigger", "True for one tick when the received byte changes")
 
       this.value = 0 // gets updated via websocket
@@ -158,7 +228,7 @@ const Op_NetReceiver = register(
 
       this._ws = new ChannelSocket(this.channel, 'listen')
       this._ws.onValue = (v) => { this.value = v }
-		}
+    }
 
     kill() {
       super.kill()
@@ -166,22 +236,26 @@ const Op_NetReceiver = register(
     }
 
     getConfig() {
-			return {
-				...super.getConfig(),
-				Channel: this.channel
-			}
-		}
+      return {
+        ...super.getConfig(),
+        Channel: this.channel
+      }
+    }
 
-		setConfig(conf, loaded = false) {
-			super.setConfig(conf, loaded)
-			if ('Channel' in conf) {
-				this.channel = conf.Channel
-				this._ws.setChannel(this.channel)
-			}
-		}
+    setConfig(conf, loaded = false) {
+      super.setConfig(conf, loaded)
+      if ('Channel' in conf) {
+        let channelValue = conf.Channel
+        if (!isValidGuid(channelValue)) {
+          channelValue = generateStringGuid(channelValue)
+        }
+        this.channel = channelValue
+        this._ws.setChannel(this.channel)
+      }
+    }
 
-		doUpdate(tick, p5ctx) {
-			super.doUpdate(tick, p5ctx)
+    doUpdate(tick, p5ctx) {
+      super.doUpdate(tick, p5ctx)
 
       this._connected = this._ws.isConnected
 
@@ -195,10 +269,12 @@ const Op_NetReceiver = register(
 
       this.out_b.value = b & 255
       this.out_t.value = !!t
-		}
+    }
 
     doDraw(tick, p5ctx) {
-			super.doDraw(tick, p5ctx)
+      super.doDraw(tick, p5ctx)
+
+      channelDisplay(this.channel, p5ctx)
 
       p5ctx.push()
 
@@ -224,10 +300,9 @@ const Op_NetReceiver = register(
         0, 0,
         30, this.height,
         p5ctx.PI * 0.5,
-        p5ctx.PI * 1.5,
-        true
+        p5ctx.PI * 1.5
       )
-    
+
       p5ctx.fill(255)
       p5ctx.stroke(0)
       p5ctx.translate(10, 0)
@@ -236,8 +311,7 @@ const Op_NetReceiver = register(
         0, 0,
         30, 40,
         p5ctx.PI * 0.5,
-        p5ctx.PI * 1.5,
-        true
+        p5ctx.PI * 1.5
       )
       p5ctx.ellipse(
         0, 0,
@@ -270,5 +344,5 @@ const Op_NetReceiver = register(
 
       p5ctx.pop()
     }
-	}
+  }
 )
